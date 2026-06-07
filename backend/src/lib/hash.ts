ea2654c61d8b1096bebbac2fc24e8977d46ee831
Gilles <contact@gillescobigo.com>
2026-06-07 22:20:53 +0200

    feat: add hash and verify password utilities

---
+++ backend/src/lib/hash.ts
@@ -0,0 +1,20 @@
+import argon2 from "argon2";
+import { isValid } from "zod/v3";
+
+export const hashPassword = async (password: string) => {
+  return await argon2.hash(password);
+};
+
+export const verifyPassword = async (password: string, hash: string) => {
+  let isValid: boolean;
+  try {
+    isValid = await argon2.verify(hash, password);
+  } catch (err) {
+    throw new Error("Erreur lors de la vérification du mot de passe");
+  }
+  if (isValid) {
+    return true;
+  } else {
+    throw new Error("Mot de passe incorrect");
+  }
+};
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,8 +1,16 @@
+import prisma from "../lib/prisma.js";
+
 export const register = async (email: string, password: string) => {
-    // Vérifier si l'email existe en BDD
-    const emailExist = 
-    // Si oui lancer une erreur
-    // Si non hasher le mdp
-    // créer le user en BDD
-    // return user sans pwd
-}
+  // Vérifier si l'email existe en BDD
+  const userExist = await prisma.user.findUnique({ where: { email } });
+
+  // Si oui lancer une erreur
+  if (userExist) {
+    return new Error();
+  }
+
+  // Si non hasher le mdp
+
+  // créer le user en BDD
+  // return user sans pwd
+};
