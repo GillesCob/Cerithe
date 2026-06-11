a9bcc85926ad4819856ba4b3bfa92ac73e96d005
Gilles <contact@gillescobigo.com>
2026-06-11 09:39:55 +0200

    feat: add register controller with error handling

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,24 @@
+// J'identifie le verbe HTTP, POST dans notre cas
+//Je récupère mail et mdp de req
+//J'envoie dans le service qui va register
+//J'appelle le service generateTokens afin de créer l'AT et Refresh
+//J'envoie le RT dans les cookies du navigateur
+//je renvoi un objet dans res avec le user et l'AT vers le front
+import type { Request, Response } from "express";
+import { register, generateTokens } from "../services/auth.service";
+
+export const registerController = async (req: Request, res: Response) => {
+  try {
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+    const newUser = await register(userEmail, userPassword);
+    const userTokens = await generateTokens(newUser.id);
+    res.cookie("refreshToken", userTokens.refreshToken, {
+      httpOnly: true,
+      secure: true,
+    });
+    return res.status(201).json({ user: newUser, accessToken: userTokens.accessToken });
+  } catch (error) {
+    return res.status(500).json({ message: "Une erreur est survenue" });
+  }
+};
