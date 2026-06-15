a12c0741ded51b5c0e3e319ca5c98eaa7b2d3c68
Gilles <contact@gillescobigo.com>
2026-06-15 13:49:50 +0200

    feat: add /me route with JWT middleware

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import { validate } from "../middlewares/validate.middleware";
 import { loginSchema, registerSchema } from "../validators/auth.validator";
-import { loginController, registerController } from "../controllers/auth.controller";
+import { loginController, meController, registerController } from "../controllers/auth.controller";
 import { Router } from "express";
+import { verifyAccessToken } from "../middlewares/auth.middleware";
 
 const authRouter = Router();
 
 authRouter.post("/register", validate(registerSchema), registerController);
 authRouter.post("/login", validate(loginSchema), loginController);
+authRouter.get("/me", verifyAccessToken, meController);
 
 export default authRouter;
