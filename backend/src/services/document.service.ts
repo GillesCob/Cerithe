d824ae8474e90c29115345670d16bd2a4d732274
Gilles <contact@gillescobigo.com>
2026-06-16 11:40:24 +0200

    feat: add document service

--- backend/src/services/document.service.ts
+++ backend/src/services/document.service.ts
@@ -1,8 +1,3 @@
-//je récupère les données provenant de supabase (path)
-//Je récupère les données provenant du controller (name, type, propertyId)
-//je create un nouveau doc avec prisma
-//je return soit l'erreur soit le doc créé via untry catch
-
 import type { documentType } from "../../prisma/generated/enums";
 import prisma from "../lib/prisma";
 
