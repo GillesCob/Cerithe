d2cf7c13f396f9ab7e4d1a973e046e400e36cf78
Gilles <contact@gillescobigo.com>
2026-06-17 10:17:15 +0200

    feat: add auth service

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,16 @@
+import apiClient from "../lib/axios";
+
+export const login = async (email: string, password: string) => {
+  const response = await apiClient.post("/api/auth/login", { email, password });
+  return response.data;
+};
+
+export const register = async (email: string, password: string) => {
+  const response = await apiClient.post("/api/auth/register", { email, password });
+  return response.data;
+};
+
+export const me = async () => {
+  const response = await apiClient.get("/api/auth/me", {});
+  return response.data;
+};
