e617c4f123907a82ada2aa4a765a5073c77c7a49
Gilles <contact@gillescobigo.com>
2026-06-29 11:08:58 +0200

    debug: log on App.tsx load

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+console.log("APP.TSX LOADED");
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
@@ -10,6 +12,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
